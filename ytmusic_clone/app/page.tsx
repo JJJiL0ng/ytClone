ed81dda0b41a4f90c2c1cdc78fdc94e33df89d2c
jjjiL0ng <qweqwe9412@gmail.com>
2024-10-03 00:46:33 +0900

    tailwind practice day2

--- ytmusic_clone/app/page.tsx
+++ ytmusic_clone/app/page.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-pink-300 h-[750px] w-[750px] flex flex-col justify-end items-start gap-4">
-      <div className="flex h-64 w-64 cursor-auto items-center justify-center bg-black text-[50px] font-[100px] text-pink-600">
-            111
+    <div className="bg-sky-500 h-[1000px] w-[1000px] flex flex-low justify-center items-ceter gap-0">
+      <div className="flex h-64 w-64 cursor-auto items-center justify-center bg-black text-[50px] font-[100px] text-blue-600">
+            hello
       </div>
       
-      <div className="flex h-64 w-64 cursor-pointer items-center justify-center bg-black text-[50px] font-[100px] text-pink-600">
-            222
+      <div className="flex h-64 w-64 cursor-pointer items-center justify-center bg-black text-[50px] font-[100px] text-orange-500">
+            it is me
       </div>
     </div>
   );
