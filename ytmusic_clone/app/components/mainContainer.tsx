5e7fcc71daf61833edbe8a034acbf19027b30507
jjjiL0ng <qweqwe9412@gmail.com>
2024-10-12 19:12:59 +0900

    main page fix

--- ytmusic_clone/app/components/mainContainer.tsx
+++ ytmusic_clone/app/components/mainContainer.tsx
@@ -1,5 +1,13 @@
 const MainContainer = () => {
-    return <div className="px-4 py-3 flex bg-green-400 rounded-2xl">메인내용을 포함</div>;
+    return
+    <div>
+      <div className="px-4 py-3 flex bg-green-400 rounded-2xl">
+        메인내용을 포함
+      </div>;
+      <div>
+        여기에 라우팅 예정
+      </div>
+      </div> 
   };
   
   export default MainContainer;
